f944a9b43341e66fdbc574c7da6a8fb90a10f9bb
lhc-dejesusron <r.dejesus@lionheart.asia>
2025-02-20 13:57:37 +0800

    working on cards

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router';
+import { useUserRecordsQuery } from '../features/record/recordsApiSlice';
+import { useEffect, useState } from 'react';
 
 const UserCard = ({ ...item }) => {
+  const [present, setPresent] = useState(false);
+
+  const { data: records } = useUserRecordsQuery(item._id);
+
+  useEffect(() => {
+    console.log(JSON.stringify(records, null, 2));
+  }, [records]);
+
   return (
     <li>
       <Link
         to={`/account/admin/${item._id}`}
-        className='card bg-base-100 w-full shadow-xl'>
+        className={`card w-full shadow-xl bg-base-100`}>
         <div className='card-body min-h-40'>
           <h2 className='card-title'>
             {item.name}
